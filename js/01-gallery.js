d3d09bd1f73db776a3d4e82cc8ff25490daade73
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-03-20 15:09:51 +0200

    Задача-1 из ДЗ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,31 +2,30 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-// galleryItems.forEach(item => {
-//   const galleryItem = document.createElement('div');
-//   galleryItem.classList.add('gallery__item');
+// //   const galleryItem = document.createElement('div');
+// //   galleryItem.classList.add('gallery__item');
 
-//   const galleryLink = document.createElement('a');
-//   galleryLink.classList.add('gallery__link');
-//   galleryLink.href = item.original;
+// //   const galleryLink = document.createElement('a');
+// //   galleryLink.classList.add('gallery__link');
+// //   galleryLink.href = item.original;
 
-//   const galleryImage = document.createElement('img');
-//   galleryImage.classList.add('gallery__image');
-//   galleryImage.src = item.preview;
-//   galleryImage.setAttribute('data-source', item.original);
-//   galleryImage.alt = item.description;
+// //   const galleryImage = document.createElement('img');
+// //   galleryImage.classList.add('gallery__image');
+// //   galleryImage.src = item.preview;
+// //   galleryImage.setAttribute('data-source', item.original);
+// //   galleryImage.alt = item.description;
 
-//   galleryItem.appendChild(galleryLink);
-//   galleryLink.appendChild(galleryImage);
+// //   galleryItem.appendChild(galleryLink);
+// //   galleryLink.appendChild(galleryImage);
 
-// gallery.appendChild(galleryItem);
-// });
+// // gallery.appendChild(galleryItem);
+// // });
 
 function createGallery(images) {
-    return images
+  return images
     .map(item => {
-    const { preview, original, description } = item;
-        return `
+      const { preview, original, description } = item;
+      return `
         <div class='gallery__item'>
           <a class='gallery__link' href=${original}>
             <img
@@ -38,7 +37,7 @@ function createGallery(images) {
           </a>
         </div>`;
     })
-        .join('');
+    .join('');
 }
 gallery.innerHTML = createGallery(galleryItems);
 
@@ -51,34 +50,36 @@ gallery.addEventListener('click', e => {
     return;
   }
 
-  const originalImgUrl = target.dataset.source;
-  const modalImg = document.createElement('img');
+  const originalImageUrl = target.dataset.source;
 
-  modalImg.src = originalImgUrl;
-  modalImg.alt = target.alt;
+  const modalImgOriginal = basicLightbox.create(`
+    <div class='modal'>
+      <img class='modal__image'
+      src='${originalImageUrl}'
+      alt='${target.alt}' 
+      />
+    </div>`);
 
-  basicLightbox.create(modalImg).show();
-
-const modalImgOriginal = basicLightbox.create(
-  `<div class='modal'>
-    <img class='modal__image'
-    alt='${target.alt}'
-    src='${originalImgUrl}'
-    />
-  </div>`,
-  {
-    onClose: () => {
-      document.removeEventListener('keydown', onEscKeyPress);
-    },
-  }
-);
-
-  const onEscKeyPress = e => {
-    if (e.code === 'Escape') {
-      modalImgOriginal.close();
-    }
-  };
-
-  document.addEventListener('keydown', onEscKeyPress);
   modalImgOriginal.show();
+
+  function closeModal() {
+    modalImgOriginal.close();
+    document.removeEventListener('keydown', handKeyPress);
+  }
+
+  const modalImage = modalImgOriginal.element().querySelector('.modal__image');
+
+  modalImage.addEventListener('click', closeModal);
+
+  function handKeyPress(e) {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+  }
+
+  document.addEventListener('keydown', handKeyPress);
+
+  modalImgOriginal.on('hidden', () => {
+    modalImage.removeEventListener('click', closeModal);
+  });
 });
